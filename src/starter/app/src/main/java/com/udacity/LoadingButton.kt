d89f5f03175ded8734839bbc72904b248eea2ed8
Tolga ÇINAR <cinarrtolga@gmail.com>
2021-02-08 12:03:00 +0000

    Button loading and Toast included

--- src/starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ src/starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,8 +3,11 @@ package com.udacity
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -12,22 +15,120 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
-
-    private val valueAnimator = ValueAnimator()
+    private var loadedPercentage = 0f
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+        when (new) {
+            ButtonState.Loading -> {
+                loadAnimation()
+                previousText = buttonText
+                buttonText = resources.getString(R.string.download_button_process_text)
+            }
+            ButtonState.Clicked -> {
+                isClickable = false
+                buttonState = ButtonState.Loading
+            }
+            ButtonState.Completed -> {
+                isClickable = true
+                buttonText = previousText
+            }
+        }
 
+        invalidate()
     }
 
+    private var buttonBackgroundColor = 0
+    private var buttonFontColor = 0
+    private var buttonTextSize = 0
+    private var buttonText = ""
+    private var previousText = ""
 
     init {
+        isClickable = true
 
+        //Custom attributes. They defined on content_main.xml and attrs.xml files.
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            buttonBackgroundColor = getColor(R.styleable.LoadingButton_backgroundColor, 0)
+            buttonFontColor = getColor(R.styleable.LoadingButton_buttonFontColor, 0)
+            buttonTextSize = getInt(R.styleable.LoadingButton_buttonFontSize, 0)
+            buttonText = getString(R.styleable.LoadingButton_buttonText).toString()
+        }
     }
 
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = buttonTextSize.toFloat()
+        color = buttonBackgroundColor
+    }
+
+    override fun performClick(): Boolean {
+        buttonState = ButtonState.Clicked
+        if (super.performClick()) return true
+
+        invalidate()
+        return true
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        drawStaticButtonShape(canvas)
 
+        if(buttonState == ButtonState.Loading) {
+            drawAnimatedButtonShape(canvas)
+        }
+
+        writeButtonText(canvas)
+    }
+
+    private fun drawStaticButtonShape(canvas: Canvas?) {
+        paint.color = buttonBackgroundColor
+        canvas!!.drawRect(
+            0.toFloat(),
+            0.toFloat(),
+            widthSize.toFloat(),
+            heightSize.toFloat(),
+            paint
+        )
+    }
+
+    private fun drawAnimatedButtonShape(canvas: Canvas?) {
+        paint.color = resources.getColor(R.color.colorPrimaryDark)
+        canvas!!.drawRect(
+                0.toFloat(),
+                0.toFloat(),
+                0 + loadedPercentage,
+                heightSize.toFloat(),
+                paint
+        )
+    }
+
+    private fun writeButtonText(canvas: Canvas?) {
+        paint.color = buttonFontColor
+        canvas!!.drawText(
+            buttonText,
+            (widthSize / 2).toFloat(),
+            ((heightSize - paint.ascent()) / 2).toFloat(),
+            paint
+        )
+    }
+
+    private fun loadAnimation() {
+        val animator = ValueAnimator.ofInt(0, 100).apply {
+            duration = 2000
+            interpolator = LinearInterpolator()
+            addUpdateListener { valueAnimator ->
+                loadedPercentage = ((valueAnimator.animatedValue as Int).toFloat() / 100) * widthSize
+
+                if(valueAnimator.animatedValue == 100) {
+                    buttonState = ButtonState.Completed
+                }
+
+                invalidate()
+            }
+        }
+
+        animator?.start()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -42,5 +143,4 @@ class LoadingButton @JvmOverloads constructor(
         heightSize = h
         setMeasuredDimension(w, h)
     }
-
 }
--- src/starter/app/src/main/java/com/udacity/MainActivity.kt
+++ src/starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -9,11 +9,12 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
+    private var selectedRadio = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +32,21 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        radio_options.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { group, checkedId ->
+            selectedRadio = when (checkedId) {
+                R.id.radio_glide -> "Glide"
+                R.id.radio_loadapp -> "LoadApp"
+                R.id.radio_retrofit -> "Retrofit"
+                else -> ""
+            }
+        })
+
         custom_button.setOnClickListener {
-            download()
+            if (selectedRadio.isNullOrEmpty()) {
+                Toast.makeText(this, resources.getString(R.string.option_required), Toast.LENGTH_SHORT).show()
+            } else {
+                download()
+            }
         }
     }
 
@@ -43,22 +58,21 @@ class MainActivity : AppCompatActivity() {
 
     private fun download() {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
-                .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
-                .setRequiresCharging(false)
-                .setAllowedOverMetered(true)
-                .setAllowedOverRoaming(true)
+                DownloadManager.Request(Uri.parse(URL))
+                        .setTitle(getString(R.string.app_name))
+                        .setDescription(getString(R.string.app_description))
+                        .setRequiresCharging(false)
+                        .setAllowedOverMetered(true)
+                        .setAllowedOverRoaming(true)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+                downloadManager.enqueue(request) // enqueue puts the download request in the queue.
     }
 
     companion object {
         private const val URL =
-            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+                "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
-
 }
